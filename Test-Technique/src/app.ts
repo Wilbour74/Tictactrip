69fd56d8cd30d8b5a12a4ac93ed0db9b1e34607b
Wilfried <wilfried.bourguignon@epitech.eu>
2023-10-29 14:01:01 +0100

    fonction pour compter mots

--- Test-Technique/src/app.ts
+++ Test-Technique/src/app.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
+const axios = require("axios")
 const app = express();
-const port = 8000;
+const path = require('path');
+const port = 7000;
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 app.get('/', (req, res) => {
-  res.send('Salut tout le monde');
+  res.sendFile(path.join(__dirname, 'app.html'))
 });
 app.listen(port, err => {
   if (err) {
@@ -11,3 +18,17 @@ app.listen(port, err => {
   }
   return console.log(`server is listening on ${port}`);
 });
+
+//Compter le nombre de mots pour les soustraires
+const countWords = (text) => {
+  const words = text.split(/\s+/)
+  const filteredWords = words.filter(word => word.trim() !== '');
+  return filteredWords.length;
+}
+
+app.post('/api/justify', (req,res) => {
+  const { justify } = req.body;
+  const wordCount = countWords(justify);
+  console.log(`Nombre de mots : ${wordCount}`);
+  
+})
