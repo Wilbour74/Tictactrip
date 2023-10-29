c239b4c6e89070d0bbfbc55ceb8755320c20a798
Wilfried <wilfried.bourguignon@epitech.eu>
2023-10-29 17:59:49 +0100

    Token fonctionnel

--- Test-Technique/dist/app.js
+++ Test-Technique/dist/app.js
@@ -5,11 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const axios = require("axios");
+const cors = require('cors');
 const app = (0, express_1.default)();
 const path = require('path');
 const port = 7000;
+app.use(cors());
 app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.text());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'app.html'));
@@ -22,13 +26,27 @@ app.listen(port, err => {
 });
 //Compter le nombre de mots pour les soustraires
 const countWords = (text) => {
-    const words = text.split(/\s+/);
-    const filteredWords = words.filter(word => word.trim() !== '');
-    return filteredWords.length;
+    if (text) {
+        const words = text.split(/\s+/);
+        const filteredWords = words.filter(word => word.trim() !== '');
+        return filteredWords.length;
+    }
+    return 0;
 };
-app.post('/console', (req, res) => {
-    const { justify } = req.body;
-    const wordCount = countWords(justify);
+//Envoie du texte au serveur
+app.post('/api/justify', (req, res) => {
+    const textTojustify = req.body;
+    const wordCount = countWords(textTojustify);
+    console.log(`Voici le texte rentré : ${textTojustify}`);
     console.log(`Nombre de mots : ${wordCount}`);
+    res.send(`Nombre de mots:  ${wordCount}`);
+});
+//Recupérer un token
+app.post('/api/token', (req, res) => {
+    const { email } = req.body;
+    const generate = Math.random().toString(36).substring(2) + Date.now().toString(36);
+    const token = jsonwebtoken_1.default.sign({ email }, generate);
+    res.json({ token });
+    console.log(token);
 });
 //# sourceMappingURL=app.js.map
--- Test-Technique/src/app.ts
+++ Test-Technique/src/app.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken'; 
 
 const axios = require("axios")
+const cors = require('cors');
 const app = express();
 const path = require('path');
 const port = 7000;
 
-app.use(bodyParser.json()); 
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.text()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.get('/', (req, res) => {
@@ -21,14 +26,32 @@ app.listen(port, err => {
 
 //Compter le nombre de mots pour les soustraires
 const countWords = (text) => {
-  const words = text.split(/\s+/)
-  const filteredWords = words.filter(word => word.trim() !== '');
-  return filteredWords.length;
+  if (text) {
+    const words = text.split(/\s+/);
+    const filteredWords = words.filter(word => word.trim() !== '');
+    return filteredWords.length;
+  }
+  return 0;
 }
 
+
+
+
+//Envoie du texte au serveur
 app.post('/api/justify', (req,res) => {
-  const { justify } = req.body;
-  const wordCount = countWords(justify);
+  const textTojustify = req.body;
+  const wordCount = countWords(textTojustify);
+  console.log(`Voici le texte rentré : ${textTojustify}`);
   console.log(`Nombre de mots : ${wordCount}`);
-  
+  res.send(`Nombre de mots:  ${wordCount}`)
+})
+
+
+//Recupérer un token
+app.post('/api/token' , (req, res) => {
+   const { email } = req.body;
+   const generate = Math.random().toString(36).substring(2) + Date.now().toString(36)
+   const token = jwt.sign({email}, generate);
+   res.json({ token });
+   console.log(token);
 })
