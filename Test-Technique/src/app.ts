6600729d6fa865c3625177af45e16c34a4f6792a
Wilfried <wilfried.bourguignon@epitech.eu>
2023-10-31 11:31:41 +0100

    Premier test

--- Test-Technique/src/app.ts
+++ Test-Technique/src/app.ts
@@ -23,6 +23,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res) => {
+  res.json({ message: "update !"})
+})
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'app.html'))
